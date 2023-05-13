22d06f100b9cf06a4889fa8360b3fc0a238f353d
Uyi Obazee <utejoe.ju@gmail.com>
2023-05-13 13:34:53 +0100

    committing tasks

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,58 +1,39 @@
 import sys
 
-def is_safe(board, row, col, n):
-    # Check if there is a queen in the same column
-    for i in range(row):
-        if board[i][col] == 1:
+def is_safe(board, row, col):
+    for i in range(col):
+        if board[i] == row or \
+           board[i] - i == row - col or \
+           board[i] + i == row + col:
             return False
-
-    # Check if there is a queen in the upper left diagonal
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check if there is a queen in the upper right diagonal
-    for i, j in zip(range(row, -1, -1), range(col, n)):
-        if board[i][j] == 1:
-            return False
-
     return True
 
-def solve(board, row, n):
-    # If all queens are placed, print the solution
-    if row == n:
-        print(board)
-        return
+def solve_n_queens(n):
+    solutions = []
+    def backtrack(board, col):
+        if col == n:
+            solutions.append(list(enumerate(board)))
+            return
+        for row in range(n):
+            if is_safe(board, row, col):
+                board[col] = row
+                backtrack(board, col+1)
+                board[col] = -1
+    backtrack([-1]*n, 0)
+    return solutions
 
-    # Try placing a queen in each column of the current row
-    for col in range(n):
-        if is_safe(board, row, col, n):
-            board[row][col] = 1
-            solve(board, row + 1, n)
-            board[row][col] = 0
-
-def nqueens(n):
-    # Check if N is at least 4
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
     if n < 4:
         print("N must be at least 4")
         sys.exit(1)
-
-    # Create an empty chessboard
-    board = [[0 for x in range(n)] for y in range(n)]
-
-    # Solve the problem
-    solve(board, 0, n)
-
-# Check if the program was called with the correct arguments
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-
-# Check if N is an integer
-try:
-    n = int(sys.argv[1])
-except ValueError:
-    print("N must be a number")
-    sys.exit(1)
-
-nqueens(n)
+    solutions = solve_n_queens(n)
+    for solution in solutions:
+        print(solution)
