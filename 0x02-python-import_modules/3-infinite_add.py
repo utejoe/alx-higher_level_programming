2529decfba5698f559e73d88e295a5819b5a58bb
Uyi Obazee <utejoe.ju@gmail.com>
2023-05-13 11:25:45 +0000

    committing tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-    """Print the addition of all arguments."""
     import sys
+    no_of_args = len(sys.argv)
+    args_sum = 0
 
-    total = 0
-    for i in range(len(sys.argv) - 1):
-        total += int(sys.argv[i + 1])
-    print("{}".format(total))
+    for i in range(1, no_of_args):
+        args_sum += int(sys.argv[i])
+    print("{:d}".format(args_sum))
