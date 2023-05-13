0887501d129e590059346b95b6174f3c776b497a
Uyi Obazee <utejoe.ju@gmail.com>
2023-05-13 14:41:25 +0100

    push

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 import sys
 
-if __name__ == "__main__":
-    argv = sys.argv
-    argc = len(argv)
+args = sys.argv
+num_args = len(args) - 1
 
-    if argc == 1:
-        print("0 arguments.")
-    else:
-        if argc == 2:
-            print("{:d} argument:".format(argc - 1))
-        else:
-            print("{:d} arguments:".format(argc - 1))
+if num_args == 0:
+    print("0 arguments.")
+else:
+    print("{} argument{}:".format(num_args, "" if num_args == 1 else "s"))
+    for i in range(1, len(args)):
+        print("{}: {}".format(i, args[i]))
+#!/usr/bin/python3
+import sys
 
-        for i in range(1, argc):
-            print("{:d}: {}".format(i, argv[i]))
+args = sys.argv[1:]
+count = len(args)
+
+if count == 0:
+    print("{:d} arguments.".format(count))
+else:
+    print("{:d} argument{}:".format(count, "" if count == 1 else "s"))
+    for i, arg in enumerate(args):
+        print("{:d}: {}".format(i+1, arg))
