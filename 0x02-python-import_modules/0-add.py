887e7f5a9dfeaff9e903b306a992fe9ee93a94cc
Uyi Obazee <utejoe.ju@gmail.com>
2023-05-24 21:35:22 +0000

    committing tasks

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
 
-a = 1
-b = 2
-result = add(a, b)
+if __name__ == "__main__":
+        a = 1
+            b = 2
+                result = add(a, b)
 
-print("{} + {} = {}".format(a, b, result))
+                    print("{:d} + {:d} = {:d}".format(a, b, result))
 
