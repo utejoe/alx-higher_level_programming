0469131e0e492ebe30bee4989f3fd261dd4b4beb
Uyi Obazee <utejoe.ju@gmail.com>
2023-06-06 09:41:29 +0000

    committing tasks

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,18 @@
 #!/usr/bin/node
-const request = require('request');
-const url = process.argv[2];
 
-request(url, function (err, data, body) {
-  if (err) {
-    console.log(err);
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
   } else {
-    let counter = 0;
     const films = JSON.parse(body).results;
-    for (let result = 0; result < films.length; result++) {
-      const characters = films[result].characters;
-      for (let j = 0; j < characters.length; j++) {
-        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
-          counter += 1;
-        }
-      }
-    }
-    console.log(counter);
+    const wedgeMovies = films.filter((film) =>
+      film.characters.includes('https://swapi-api.alx-tools.com/api/people/18/')
+    );
+    console.log(wedgeMovies.length);
   }
 });
+
